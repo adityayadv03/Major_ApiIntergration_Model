bd38eb5000edb350dc2bc480749ffba15cd16bf3
Aditya Yadav <officialuser.aditya2022@gmail.com>
2025-12-07 19:18:19 +0530

    CORS Error Fixing

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",   # your static HTML origin
+    "http://localhost:5173",   # optional, if you also use Vite later
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     # or ["*"] while testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load the trained model
 model = tf.keras.models.load_model("model.h5")  # rename your h5 file to model.h5
